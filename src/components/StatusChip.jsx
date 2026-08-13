980463767a8b9d2ec805f0a92553011b82812458
Anish-45 <karthikanish45@gmail.com>
2026-08-14 01:20:44 +0530

    Delete src/components directory

--- src/components/StatusChip.jsx
+++
@@ -1,9 +0,0 @@
-import { STATUS_LABELS } from '../lib/constants'
-
-export default function StatusChip({ status }) {
-  let cls = 'chip'
-  if (status === 'DELIVERED') cls += ' delivered'
-  else if (status === 'EXCEPTION') cls += ' exception'
-  else if (['IN_FLIGHT', 'LOADED', 'LANDED', 'OUT_FOR_DELIVERY'].includes(status)) cls += ' transit'
-  return <span className={cls}>{STATUS_LABELS[status] ?? status}</span>
-}
